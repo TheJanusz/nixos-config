06d268387869871b6c32f6a8ccf1e9702157a21a
TheJanusz <pietrzyk.janusz1@gmail.com>
2026-06-26 23:55:39 +0200

    add homelab to agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,14 +1,15 @@
 
 let
   system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBqTZ/frLgist2Q5CKKUs1+0lpJ6TLAejdMH92DRq/U7 root@nixos";
+  homelab = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMVAb8zagL2ZQz58Fe6UiGKbkplI3tk77QAGx/6JcRwf root@nixos";
   systems = [ system1 ];
   user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvJ56LJONXj+4+WBYzJoo7Ohxl2PPZD5zLNzpAcu9ST pietrzyk.janusz1@gmail.com";
   users = [ user1 ];
 in {
-  "secret1.age".publicKeys = [ user1 system1 ];
-  "wg-privatekey.age".publicKeys = [ user1 system1 ];
-  "authentik-env.age".publicKeys = [ user1 system1 ];
-  "wildcard.internal.crt.age".publicKeys = [ user1 system1 ];
-  "wildcard.internal.key.age".publicKeys = [ user1 system1 ];
-  "wg-peers.conf.age".publicKeys = [ user1 system1 ];
+  "secret1.age".publicKeys = [ user1 system1 homelab ];
+  "wg-privatekey.age".publicKeys = [ user1 system1 homelab ];
+  "authentik-env.age".publicKeys = [ user1 system1 homelab ];
+  "wildcard.internal.crt.age".publicKeys = [ user1 system1 homelab ];
+  "wildcard.internal.key.age".publicKeys = [ user1 system1 homelab ];
+  "wg-peers.conf.age".publicKeys = [ user1 system1 homelab ];
 }

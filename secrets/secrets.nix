0942c553e7c3b39e6b52f1f2d959dce66dfe806c
TheJanusz <pietrzyk.janusz1@gmail.com>
2026-07-30 01:10:52 +0200

    include homelab in secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -12,5 +12,5 @@ in {
   "wildcard.internal.crt.age".publicKeys = [ user1 system1 homelab ];
   "wildcard.internal.key.age".publicKeys = [ user1 system1 homelab ];
   "wg-peers.conf.age".publicKeys = [ user1 system1 homelab ];
-  "wg-client-privatekey.age".publicKeys = [ user1 system1 ];
+  "wg-client-privatekey.age".publicKeys = [ user1 system1 homelab ];
 }
